033091cd43c85a75dbcf8b0d5623c4b27bd0f4a1
MoryBinM <101923228+MoryBinM@users.noreply.github.com>
2024-05-17 03:13:02 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="MoryApps" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by editing <code>F**K You.js</code>
         </p>
       </main>
 
